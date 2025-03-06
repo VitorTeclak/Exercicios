018d02736b6695455f2894bdf1b5fd20e2fd9880
VitorTeclak <vitorteclak@hotmail.com>
2025-03-06 19:51:24 -0300

    Correção

--- ProjetoConcessionaria/entities/cliente.py
+++ ProjetoConcessionaria/entities/cliente.py
@@ -27,7 +27,7 @@ class Cliente:
             connection = pymysql.connect(
                 host="localhost",
                 user="root",
-                password="1421",
+                password="senha",
                 database="concessionaria",
             )
 
@@ -81,7 +81,7 @@ class Cliente:
         connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="1421",
+        password="senha",
         database="concessionaria"
 )
 
@@ -109,7 +109,7 @@ class Cliente:
         connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="1421",
+        password="senha",
         database="concessionaria"
 )
 
@@ -152,7 +152,7 @@ class Cliente:
         connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="1421",
+        password="senha",
         database="concessionaria")
         try:
             with connection.cursor() as cursor:
--- ProjetoConcessionaria/entities/veiculo.py
+++ ProjetoConcessionaria/entities/veiculo.py
@@ -21,7 +21,7 @@ class veiculo:
             connection = pymysql.connect(
                 host="localhost",
                 user="root",
-                password="1421",
+                password="senha",
                 database="concessionaria",
             )
 
@@ -72,7 +72,7 @@ class veiculo:
         connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="1421",
+        password="senha",
         database="concessionaria"
 )
 
@@ -99,7 +99,7 @@ class veiculo:
         connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="1421",
+        password="senha",
         database="concessionaria"
 )
 
@@ -143,7 +143,7 @@ class veiculo:
         connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="1421",
+        password="senha",
         database="concessionaria")
         try:
             with connection.cursor() as cursor:
@@ -198,7 +198,7 @@ class veiculo:
         connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="1421",
+        password="senha",
         database="concessionaria")
         try:
             with connection.cursor() as cursor:
